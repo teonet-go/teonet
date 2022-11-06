8495755905ee7c17d2c25d16bb86349e429fd282
Kirill Scherba <kirill@scherba.ru>
2022-11-06 13:45:57 +0300

    Create teocom application folder and main file

---
+++ cmd/teocom/main.go
@@ -0,0 +1,21 @@
+// Copyright 2022 Kirill Scherba <kirill@scherba.ru>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Teonet Command - teonet command line interface application which connect to 
+// selected teonet peer and execute it command
+package main
+
+func main() {
+	// TODO: Show application logo
+
+	// TODO: Parse parameters
+
+	// TODO: Connect to teonet
+
+	// TODO: Connect to selected peer
+
+	// TODO: Execute command 
+
+	// TODO: Return result
+}
