b91ab1bbbe0b87d2e46e84f35a9d55f237021eee
Kirill Scherba <kirill@scherba.ru>
2022-11-06 13:48:46 +0300

    Application logo created

--- cmd/teocom/main.go
+++ cmd/teocom/main.go
@@ -2,12 +2,21 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Teonet Command - teonet command line interface application which connect to 
+// Teonet Command - teonet command line interface application which connect to
 // selected teonet peer and execute it command
 package main
 
+import "github.com/teonet-go/teonet"
+
+const (
+	appName    = "Teonet command application"
+	appShort   = "teocom"
+	appVersion = teonet.Version
+)
+
 func main() {
-	// TODO: Show application logo
+	// Application logo
+	teonet.Logo(appName, appVersion)
 
 	// TODO: Parse parameters
 
@@ -15,7 +24,7 @@ func main() {
 
 	// TODO: Connect to selected peer
 
-	// TODO: Execute command 
+	// TODO: Execute command
 
 	// TODO: Return result
 }
